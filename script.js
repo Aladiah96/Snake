5b25f95db561ca4ca6a286199cdf6a6269ef73e7
Aladiah96 <63356947+Aladiah96@users.noreply.github.com>
2021-11-16 20:45:53 -0300

    teste

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillstyle = "lightgreen";
@@ -20,7 +24,26 @@ function criarCobrinha() {
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect()
+}
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction !="up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15* box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake [0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -39,7 +62,7 @@ function iniciarJogo(){
         y: snakeY
     }
 
-    snake.unshift(newHead)
+    snake.unshift(newHead);
 
 }
 
