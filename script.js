09b9755df5baf804c5beb135ea0f827c7b4f7641
Aladiah96 <63356947+Aladiah96@users.noreply.github.com>
2021-11-16 21:16:03 -0300

    definir aleatoriedade da fruta e aumentar a cobra

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ snake[0] = {
 let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(math.random() * 15 + 1) * box
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 function criarBG() {
-    context.fillstyle = "lightgreen";
-    context.fillrect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
@@ -26,7 +26,7 @@ function criarCobrinha() {
 
 function drawFood(){
     context.fillStyle = "red";
-    context.fillRect()
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update);
@@ -41,11 +41,12 @@ function update (event){
 function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15* box && direction == "down") snake[0].y = 0;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake [0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -55,7 +56,12 @@ function iniciarJogo(){
     if(direction == 'up') snakeY -= box;
     if(direction == 'down') snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else {food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
 
     let newHead = {
         x: snakeX,
